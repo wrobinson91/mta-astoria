a28f4eb9b5ef93049ac48814386710bdbb346ef2
Will Robinson <wrobinson91@gmail.com>
2019-07-02 16:18:51 -0400

    hmr added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <h2>This Is My Project, Foo!</h2>
+  <h2>This Is My Project</h2>
 );
 
 export default App;
--- client/index.js
+++ client/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 // needs to have JSX
+import { AppContainer } from 'react-hot-loader';
 import App from './components/App.jsx';
-// import
+// import styles from './styles/styles.css';
 
 render(
-  <App />,
+  <AppContainer>
+    <App />
+  </AppContainer>,
   document.getElementById('root'),
 );
+
+if (module.hot) {
+  module.hot.accept('./components/App.jsx', () => {
+    const NextApp = require('./components/App.jsx').default;
+    render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+    );
+  });
+}
